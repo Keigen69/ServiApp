959b4959aa678abd6390ae2f7dfbce6a5de617e4
Keigen69 <ignacioanacona@gmail.com>
2017-12-26 14:32:40 -0300

    pag

---
+++ src/pages/popover/popover.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ViewController } from 'ionic-angular';
+
+/**
+ * Generated class for the PopoverPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+     template: `
+    <ion-list>
+      <ion-list-header>Ionic</ion-list-header>
+      <button ion-item (click)="close()">Learn Ionic</button>
+      <button ion-item (click)="close()">Documentation</button>
+      <button ion-item (click)="close()">Showcase</button>
+      <button ion-item (click)="close()">GitHub Repo</button>
+    </ion-list>
+  `
+  
+})
+export class PopoverPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+  }
+
+  close() {
+    this.viewCtrl.dismiss();
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PopoverPage');
+  }
+
+}
