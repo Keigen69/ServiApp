7ad7f7336f11c9ffac0d5bbbfc3eed89d36fa40e
shadowlighty <shadow_lighty@hotmail.com>
2017-12-26 23:23:36 -0300

    poporia

--- src/pages/popover/popover.ts
+++ src/pages/popover/popover.ts
@@ -11,15 +11,8 @@ import { ViewController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-     template: `
-    <ion-list>
-      <ion-list-header>Ionic</ion-list-header>
-      <button ion-item (click)="close()">Learn Ionic</button>
-      <button ion-item (click)="close()">Documentation</button>
-      <button ion-item (click)="close()">Showcase</button>
-      <button ion-item (click)="close()">GitHub Repo</button>
-    </ion-list>
-  `
+      selector: 'page-popover',
+  templateUrl: 'popover.html',
   
 })
 export class PopoverPage {
